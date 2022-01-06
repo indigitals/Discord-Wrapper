fef9f5d7f3d1cbd995dfd2dc9fa91f132ba4eb15
indigitals <96959296+indigitals@users.noreply.github.com>
2022-01-05 21:45:13 -0600

    connecting fully works

--- aiosocket.py
+++ aiosocket.py
@@ -1,24 +1,25 @@
-from socket import SocketKind
-import aiohttp
+#from socket import SocketKind
+import websockets
 import random
 import asyncio
 import json
 from opcodes import * 
 
-DISPATCH = 0
-HEARTBEAT = 1
-IDENTIFY = 2
-STATUS_UPDATE = 3
-VOICE_UPDATE = 4
-RESUME = 6
-RECONNECT = 7
-REQUEST_MEMBERS = 8
-INVALID_SESSION = 9
-HELLO = 10
-HEARTBEAT_ACK = 11
+
 
 
 class SocketConn:
+    DISPATCH = 0
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    STATUS_UPDATE = 3
+    VOICE_UPDATE = 4
+    RESUME = 6
+    RECONNECT = 7
+    REQUEST_MEMBERS = 8
+    INVALID_SESSION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
     def __init__(self, token: str):
         self.token = token
         self.info = BaseSocketInfo()
@@ -27,53 +28,34 @@ class SocketConn:
         self.sequence = None
         self.ready = False
         self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
-        self.session = self._await(self.initSession())
-        self.ws = self._await(self.initWS(
-            **{
-            "max_msg_size": 0,
-            #"timeout": 30.0,
-            "autoclose": False,
-            "headers": {"User-Agent": self.user_agent},
-            "compress": 0,
-            }
-        ))
+        self.ws = self._await(self.initWS())
 
-    async def initSession(self):
-        return aiohttp.ClientSession()
-
-    async def initWS(self, **kwargs):
-        return await self.session.ws_connect("wss://gateway.discord.gg/?encoding=json&v=9", **kwargs)
+    async def initWS(self):
+       return await websockets.connect("wss://gateway.discord.gg/?encoding=json&v=9")
 
     async def send(self, payload):
-        return await self.ws.send_json(payload)
-
-    async def recv(self):
-        return await self.ws.receive_json()
+        return await self.ws.send(payload)
 
     async def send_identify(self):
-        await self.send(self.info.returnIdentity(self.token, "linux", self.user_agent, "pc"))
-        self.interval = (await self.recv())["d"]["heartbeat_interval"]/1000.0 
-        #print(self.interval)
+        await self.send(self.info.returnIdentity(self.token))
+        self.interval = (json.loads(await self.ws.recv()))["d"]["heartbeat_interval"]/1000.0 
 
     async def keep_conn(self):
-        print(self.interval)
         while self.interval is not None:
-            print('Sending Heartbeat')
-            print(self.sequence)
             await self.send(self.info.returnHeartbeat(self.sequence if self.sequence is not None else 'null'))
             await asyncio.sleep(self.interval)
 
     async def check_ready(self):
-        print("Check Ready")
-        for message in await self.recv():
-            print("< {}".format(message))
+
+        print("Entering receive")
+        async for message in self.ws:
+            #print("<", message)
             data = json.loads(message)
-            if data["op"] == DISPATCH:
+            if data["op"] == self.DISPATCH:
                 self.sequence = int(data["s"])
-                event_type = data["t"]
-                if event_type == "READY":
-                    self.session_id = data["d"]["session_id"]
-                    print("Got session ID:", self.session_id)
+                if data["t"] == "READY":
+                    print("Got session ID: {}".format(data["d"]["session_id"]))
+
 
     async def tempmain(self):
         await self.send_identify()
@@ -82,5 +64,5 @@ class SocketConn:
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(SocketConn("ODY3MDk1MTMxMDM1MjcxMTg4.YdZHyg.0_yg2og_yNu-sPYWXDs5UOxh4O0").tempmain())
+    loop.run_until_complete(SocketConn("ODY3MDk1MTMxMDM1MjcxMTg4.YdZLjg.ockCWzQUJhTvc4ohN0JrJ7ePxZU").tempmain())
 
--- opcodes.py
+++ opcodes.py
@@ -10,19 +10,15 @@ class BaseSocketInfo:
         })
     def returnIdentity(self, token: str, os: str = "linux", browser: str = "Chrome", device: str = "pc"):
         return self.createPayload(2, 
-        {
-            "token": token,
-            "properties": {
-                "$os": os,
-                "$browser": browser,
-                "$device": device,
-                "$referrer": "",
-                "$referring_domain": "",
-            },
-            "compress": False,
-            "large_threshold": 250,
-            "v": 3,
-        })
+{
+        "token": token,
+        "properties": {
+            "$os": "windows",
+            "$browser": "chrome",
+            "$device": 'pc'
+        }
+        
+})
 
     def returnHeartbeat(self, sequence):
         return self.createPayload(1, sequence)
