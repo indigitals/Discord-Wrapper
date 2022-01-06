dabc50561d23768068e954d1363525fce58e91ca
indigitals <96959296+indigitals@users.noreply.github.com>
2022-01-06 09:35:21 -0600

    var/file renames + heartbeat run in background

--- init_connection.py
+++ init_connection.py
@@ -8,7 +8,7 @@ from opcodes import *
 
 
 
-class SocketConn:
+class Connect:
     DISPATCH = 0
     HEARTBEAT = 1
     IDENTIFY = 2
@@ -25,10 +25,14 @@ class SocketConn:
         self.info = BaseSocketInfo()
         self.loop = asyncio.get_event_loop()
         self._await = self.loop.run_until_complete
+        self._forever = self.loop.run_forever
         self.sequence = None
         self.ready = False
-        self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+        self.session_id = None
+        self.interval = None
+        self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36" #might use later otherwise redundant afaik
         self.ws = self._await(self.initWS())
+        self._await(self.send_heartbeat())
 
     async def initWS(self):
        return await websockets.connect("wss://gateway.discord.gg/?encoding=json&v=9")
@@ -37,32 +41,36 @@ class SocketConn:
         return await self.ws.send(payload)
 
     async def send_identify(self):
+        #print("sent")
         await self.send(self.info.returnIdentity(self.token))
         self.interval = (json.loads(await self.ws.recv()))["d"]["heartbeat_interval"]/1000.0 
 
-    async def keep_conn(self):
+    async def send_heartbeat(self):
         while self.interval is not None:
             await self.send(self.info.returnHeartbeat(self.sequence if self.sequence is not None else 'null'))
+            print("sent heartbeat")
             await asyncio.sleep(self.interval)
 
-    async def check_ready(self):
 
-        print("Entering receive")
+    async def check_ready(self):
+        #print("Entering receive")
         async for message in self.ws:
             #print("<", message)
             data = json.loads(message)
             if data["op"] == self.DISPATCH:
                 self.sequence = int(data["s"])
                 if data["t"] == "READY":
-                    print("Got session ID: {}".format(data["d"]["session_id"]))
+                    self.session_id = data["d"]["session_id"]
+                    return
 
 
     async def tempmain(self):
         await self.send_identify()
-        await asyncio.gather(self.keep_conn(), self.check_ready())
+        await asyncio.gather(self.check_ready())
+        print(self.session_id)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(SocketConn("ODY3MDk1MTMxMDM1MjcxMTg4.YdZLjg.ockCWzQUJhTvc4ohN0JrJ7ePxZU").tempmain())
+    loop.run_until_complete(Connect("").tempmain())
 
