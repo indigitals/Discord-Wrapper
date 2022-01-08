cbea33f910a5270caafa1bde5668415851bd1f7a
indigitals <96959296+indigitals@users.noreply.github.com>
2022-01-08 15:40:35 -0600

    allow me to cancel receive messages when done

--- voice_connection.py
+++ voice_connection.py
@@ -7,7 +7,7 @@ class VC:
         self.connect = Connection(token)
 
     async def voice_update(self): #send the op4 voice state update
-        await self.connect.send(self.connect.info.returnVoiceUpdate())
+        await self.connect.send(self.connect.info.returnVoiceUpdate(self.gid, self.cid))
 
     async def start_vc_messages(self): #receive responses from the op4 that will be sent out (expecting 2 then cancel)
         while True:
@@ -24,13 +24,14 @@ class VC:
 
 
     async def _init_connection(self):
-        #msg_task = self.connect._task(self.connect.msg_handler()) no point getting messages from the main gateway but why not
-        self.connect.start() #initialize connection to main gateway
+        msg_task = self.connect._task(self.connect.msg_handler()) #no point getting messages from the main gateway but why not
+        await asyncio.gather(self.connect.send_heartbeat(), self.connect.send_identify(), msg_task) #initialize connection to main gateway
 
     async def init_vc(self):
-        await asyncio.gather(self.voice_update, self.start_vc_messages())
+        await self._init_connection()
+        await asyncio.gather(self.voice_update(), self.start_vc_messages())
         print(self.token, self.endpoint, self.vc_session)
 
 if __name__=="__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(VC("926987966755266560", "926877164165541908", "ODY3MDk1MTMxMDM1MjcxMTg4.YdZsHw.BlD1ZWszlT03O0bPGfqp_BxiGJk").init_vc())
+    loop.run_until_complete(VC("926987966755266560", "926877164165541908", "").init_vc())
